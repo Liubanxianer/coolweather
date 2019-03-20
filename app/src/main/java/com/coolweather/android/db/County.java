4f65c8c3f74f57890b513281c579eec402379ca8
Liubanxianer <liubanxian.chao@gmail.com>
2019-03-20 21:52:32 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -0,0 +1,46 @@
+package com.coolweather.android.db;
+
+import DataSupport;
+
+/**
+ * Created by MSE on 2019/3/20.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private  String CountyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCountyName() {
+        return CountyName;
+    }
+
+    public void setCountyName(String countyName) {
+        CountyName = countyName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
